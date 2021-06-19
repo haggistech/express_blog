f662bab6f18d8f8104e7c8e37398196781c9c80d
haggistech <haggistech@gmail.com>
2021-06-19 20:23:58 +0100

    Sign Up Working

--- routes/articles.js
+++ routes/articles.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Article = require('./../models/article')
+const User = require('./../models/users')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
