336b38f911377cf72abeea327236591869499a48
haggistech@gmail.com <haggistech@gmail.com>
2021-10-07 14:57:21 +0100

    blank env

--- config/passport.js
+++ config/passport.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 
-const Users = mongoose.model('Users');
-
 passport.use(new LocalStrategy({
   usernameField: 'user[email]',
   passwordField: 'user[password]',
